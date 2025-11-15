08d41db8a3b5efee75d6dc1671120c4fbf44c14e
Vx-tech <xaviervianney8@gmail.com>
2025-11-15 16:05:48 +0300

    Setting up the User card interface and component

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -4,3 +4,26 @@ export interface PostProps {
   title: string;
   body: string;
 }
+
+export interface UserProps {
+    name: string;
+    username: string;
+    email: string;
+    address:{
+        street: string;
+        suite: string;
+        city: string;
+        zipcode: string;
+        geo: {
+            lat: number;
+            lng: number;
+        }
+    };
+    phone: number;
+    website: string;
+    company: {
+        name: string;
+        catchPhrase:string;
+        bs: string;
+    }
+}
